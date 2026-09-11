2a1d4f5a091ad98e72f88bcdcfdd3eace1562c00
PajgerZs <pajgerzs@kkszki.hu>
2026-09-11 22:28:51 +0200

    1. feladat megoldása

--- OopGyakorlas/Program.cs
+++ OopGyakorlas/Program.cs
@@ -10,6 +10,12 @@ namespace OopGyakorlas
     {
         static void Main(string[] args)
         {
+            // 1. Feladat: Példányosítás és kiírás
+            Szemely sz1 = new Szemely();
+            sz1.Nev = "Kovács Péter";
+            sz1.Eletkor = 20;
+
+            Console.WriteLine($"Név: {sz1.Nev}, Életkor: {sz1.Eletkor}");
         }
     }
 }
